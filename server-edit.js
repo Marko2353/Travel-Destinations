c749bf723043e58235686fa47aee568ee318bbe7
ZuRi-To <91980222+ZuRi-To@users.noreply.github.com>
2022-09-24 16:19:22 +0200

    update
    not working

--- server-edit.js
+++ server-edit.js
@@ -26,7 +26,7 @@ const Destination = mongodb.model("destinations", destinationsSchema);
 
 app.get('/', (req, res) => {
   Destination.find({}, function (err, destinations) {
-    res.render('index', {
+    res.render('index-edit', {
       destinationsList: destinations
     })
   })
@@ -65,6 +65,13 @@ app.post("/", function (req, res) {
   newDestination.save();
   res.redirect('/');
 })
+
+var query = {'_id': '632f0908b66bb4d51cb1e838'};
+
+MyModel.findOneAndUpdate(query, req.newData, {upsert: true}, function(err, doc) {
+  if (err) return res.send(8000, {error: err});
+  return res.send('Succesfully saved.');
+});
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000!")
